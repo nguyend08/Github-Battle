b7e5546517ca4e7274a027806c2d8723e3e3b600
Daniel <dahnguyen@ucdavis.edu>
2019-08-02 18:42:39 -0700

    created first component, added button styling

---
+++ github-battle/app/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from '../components/Popular'
+
+//Component
+//State
+//Lifecycle
+//UI
+
+class App extends React.Component {
+  render() {
+    return ( 
+      <div className = "container">
+        <Popular />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+
+  //React Element,
+  //Where to render the element to
+
+  <App /> , document.getElementById('app')
+)
