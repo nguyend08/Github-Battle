e8f76daf00a7fdab6f5caa78aa1b2f1bacebd733
Daniel <dahnguyen@ucdavis.edu>
2019-08-05 12:04:43 -0700

    added red highlight to selected language on popular tab

--- github-battle/app/components/Popular.js
+++ github-battle/app/components/Popular.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			selectedLangauge: 'All'
+		}
+
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+	
+	updateLanguage(selectedLangauge) {
+		this.setState ({
+			selectedLangauge : selectedLangauge
+		})
+	}
 	render() {
 		const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 		return ( 
 			<ul className = 'flex-center' > {
 				languages.map((language) => ( 
 					<li key={language}>
-						<button className ='btn-clear nav-link'>
+						<button 
+						className ='btn-clear nav-link'
+						style={language === this.state.selectedLangauge ? { color: 'rgb(187,46,31)' } : null } 
+						onClick={() => this.updateLanguage(language)}
+						>
 							{language}
 						</button>
 					</li>
