1b1974af01cae2695198bda0cab047ad0f5f75fc
Daniel <dahnguyen@ucdavis.edu>
2019-08-19 21:30:11 -0700

    converted ProfileList back to functional component, added Tooltip component

--- github-battle/app/components/Results.js
+++ github-battle/app/components/Results.js
@@ -34,72 +34,35 @@ const styles = {
   }
 }
 
-class ProfileList extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hoveringLocation: false,
-      hoveringCompany: false
-    }
-    this.mouseOut = this.mouseOut.bind(this);
-    this.mouseOver = this.mouseOver.bind(this);
-  }
-  mouseOver(id) {
-    this.setState({
-      [id]: true
-    })
-  }
-
-  mouseOut(id) {
-    this.setState({
-      [id]: false
-    })
-  }
-
-  render() {
-    const { profile } = this.props;
-    const { hoveringCompany, hoveringLocation } = this.state;
-
-    return(
-      <ul className="card-list">
-        <li>
-          <FaUser color="rgb(239, 115, 115)" size={22} />
-          {profile.name}
-        </li>
-        {profile.location && (
-          <li 
-            onMouseOver={() => this.mouseOver('hoveringLocation')}
-            onMouseOut={() => this.mouseOut('hoveringLocation')}
-            style={styles.container}
-          >
-            {hoveringLocation === true && <div style={styles.tooltip}>User's location</div>}
-            <FaCompass color="rgb(144, 115, 255)" size={22} />
-            {profile.location}
-          </li>
-        )}
-        {profile.company && (
-          <li
-            onMouseOver={() => this.mouseOver('hoveringCompany')}
-            onMouseOut={() => this.mouseOut('hoveringCompany')}
-            style={styles.container}
-          >
-            {hoveringCompany === true && <div style={styles.tooltip}>User's company</div>}
-            <FaBriefcase color="#795548" size={22} />
-            {profile.company}
-          </li>
-        )}
-        <li>
-          <FaUsers color="rgb(129, 195, 245)" size={22} />
-          {profile.followers.toLocaleString()} followers
-        </li>
-        <li>
-          <FaUserFriends color="rgb(64, 183, 95)" size={22} />
-          {profile.following.toLocaleString()} following
-        </li>
-      </ul>
-      )
-  }
+function ProfileList({ profile }) {
+  return(
+  <ul className="card-list">
+    <li>
+      <FaUser color="rgb(239, 115, 115)" size={22} />
+      {profile.name}
+    </li>
+    {profile.location && (
+      <li>
+        <FaCompass color="rgb(144, 115, 255)" size={22} />
+        {profile.location}
+      </li>
+    )}
+    {profile.company && (
+      <li>
+        <FaBriefcase color="#795548" size={22} />
+        {profile.company}
+      </li>
+    )}
+    <li>
+      <FaUsers color="rgb(129, 195, 245)" size={22} />
+      {profile.followers.toLocaleString()} followers
+    </li>
+    <li>
+      <FaUserFriends color="rgb(64, 183, 95)" size={22} />
+      {profile.following.toLocaleString()} following
+    </li>
+  </ul>
+  )
 }
 
 ProfileList.propTypes = {
